f7c0e61eec543cd3fcc6709be27cd611fc1c7e88
Colin Fry <colin.fry@tribalworldwide.co.uk>
2022-03-22 21:15:27 +0000

    Initial Commit

---
+++ uMotif-ChuckNorris/uMotif_ChuckNorrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  uMotif_ChuckNorrisApp.swift
+//  uMotif-ChuckNorris
+//
+//  Created by Colin Fry on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct uMotif_ChuckNorrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
